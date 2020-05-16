4bbb72205cb7d27e1946ea4f4d13f3f7a83416c2
Sebastien Lavoie <slavoie689@gmail.com>
2020-05-16 15:55:30 -0400

    don't log to file by default

--- logger.py
+++ logger.py
@@ -3,22 +3,24 @@ import logging.handlers
 
 
 class Logger:
-    def __init__(self):
+    def __init__(self, file_handler: bool):
         LOGFILE = f'solver.log'
         logger = logging.getLogger("")
         logger.addHandler(logging.StreamHandler())
         logger.setLevel(logging.DEBUG)
 
-        formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
-        handler = logging.handlers.RotatingFileHandler(
-            LOGFILE, maxBytes=(10*1024*1024), backupCount=7
-        )
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
+        if file_handler:
+            formatter = logging.Formatter("%(asctime)s - %(filename)s - %(levelname)s - %(message)s")
+            handler = logging.handlers.RotatingFileHandler(
+                LOGFILE, maxBytes=(10*1024*1024), backupCount=7
+            )
+            handler.setFormatter(formatter)
+            handler.setLevel(logging.DEBUG)
+            logger.addHandler(handler)
         self.logger = logger
 
 
-logger = Logger().logger
+logger = Logger(file_handler=False).logger
 logger.setLevel(logging.DEBUG)
 
 
